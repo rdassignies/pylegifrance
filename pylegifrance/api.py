523f2d69d8cf3c5118d0ef6236fe2f159ef01b54
rdassignies <93215594+rdassignies@users.noreply.github.com>
2023-06-19 11:50:46 +0200

    dynamics functions with routes

--- pylegifrance/api.py
+++ pylegifrance/api.py
@@ -10,13 +10,21 @@ import requests
 import time
 import json
 from uuid import uuid4
+import inspect
+from inspect import Parameter
+
 from requests_oauthlib import OAuth2Session
-from pylegifrance.models import routes
 
 _API_HOST = 'https://api.aife.economie.gouv.fr/dila/legifrance-beta/lf-engine-app'
 _TOKEN_URL = 'https://oauth.piste.gouv.fr/api/oauth/token'
 
-class LegifranceClient:
+"""
+load_dotenv()
+client_id = os.getenv("OAUTH_CLIENT_ID")
+client_secret = os.getenv("OAUTH_CLIENT_SECRET")
+"""
+
+class LegiHandler:
     
     def __init__(self):
         self.token = ''
@@ -26,7 +34,21 @@ class LegifranceClient:
         self.api_url = 'https://api.piste.gouv.fr/dila/legifrance/lf-engine-app/'
         self.docs = [] #stock les dictionnaires de metadonnées
         self.time_token = time.time()
-        self.token = self._get_access_token()
+        self._get_access_token()
+        
+        self.post_headers = {
+            'Authorization': f'Bearer {self.token}',
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+        
+        from models import routes
+        self.routes = routes
+        
+        for route in routes.keys():
+            method_name = route.replace('/', '_').strip('_')  # Convertir la route en nom de méthode
+            method = self._create_method(route)
+            setattr(self, method_name, method)
         
         #with open(config_file, 'r') as f:
         #    self.config = json.load(f)
@@ -39,9 +61,11 @@ class LegifranceClient:
             'scope': 'openid'
         }
         response = requests.post(self.token_url, data=data)
-        self.token = response.json().get('access_token')
-        #response.raise_for_status()
-        #
+        if 200 <= response.status_code < 300:
+                self.token = response.json().get('access_token')
+        else : 
+            raise Exception(f"Erreur lors de l'obtention du token erreur : {response.status_code}")
+        
     
     
     def _update_client(self) : 
@@ -49,7 +73,51 @@ class LegifranceClient:
         if elapsed_time >= 3600 : 
             self.time_token = time.time()
             self.token = self._get_access_token()
+            
+            
+    def _create_method(self, route):
+        # Créer une nouvelle méthode qui appelle l'API avec les données fournies
+        def method(**data):
+            # Utiliser les valeurs par défaut de la route si une clé n'est pas présente dans les données
+            default_data = {key: data.get(key, value) for key, value in self.routes[route].items()}
+            # Si data est vide, utiliser default_data
+            if not data:
+                data = default_data
+            return self.call_api(route, data)
+        
+        # Créer une nouvelle signature pour la méthode avec les paramètres de la route
+        params = [Parameter(key, Parameter.KEYWORD_ONLY, default=value) for key, value in self.routes[route].items()]
+        method.__signature__ = inspect.Signature(params)
+
+        # Ajouter une docstring à la méthode
+        method.__doc__ = f"Appelle l'API avec la route '{route}' et les données suivantes : {self.routes[route]}"
+
+        return method
+
+
+    def _validate_data(self, route, data):
+        # Vérifiez que toutes les clés nécessaires sont présentes
+        for key in self.routes[route].keys():
+            if key not in data:
+                raise ValueError(f"La clé '{key}' est manquante dans" 
+                                 "les données fournies pour la route {route}")
+            if not data[key]:
+                raise ValueError(f"La clé '{key}' n'a pas de valeur associée"
+                                 "dans les données fournies pour la route {route}")
+   
+        
     
+    def call_api(self, route, data):
+       
+        self._validate_data(route, data)
+        response = requests.post(f"{self.api_url}{route}",headers=self.post_headers, json=data)
+        
+        if response.status_code >= 400 and response.status_code < 500:
+            raise Exception(f"Erreur client  {response.status_code} lors de l'appel à l'API :")
+  
+        return response
+    
+
     def ping(self, route:str = "list/ping") : 
         pass
 
@@ -63,7 +131,7 @@ class LegifranceClient:
         
         return response
 
-    def post(
+    def _post(
             self, 
             payload:dict, 
             route:str="search",
--- pylegifrance/models.py
+++ pylegifrance/models.py
@@ -17,124 +17,30 @@ routes = {
         "searchedString": "",
         "date": "",
         "textId": ""
-      }
+      }, 
+      "/consult/juri": 
+      {
+       "textId": "JURITEXT000037999394",
+       "searchedString": "constitution 1958"
+       }, 
+      "/consult/jorf" : 
+      {
+       "textCid": "JORFTEXT000033736934",
+       "searchedString": "constitution 1958"
+       },
+      "/list/dossiersLegislatifs" : 
+         {
+             "legislatureId": 15,
+             "type": "LOI_PUBLIEE"
+         },
+    "/suggest" : 
+        {
+            "supplies": [
+                "JORF",
+                "JURI"
+        ],
+        "searchText": "mariage"
+        }
+        
     }
 
-liste_code = {
-  "route" : "list/code",
-      'payload' : {
-      "states": [
-        "VIGUEUR",
-        "ABROGE",
-        "VIGUEUR_DIFF"
-      ],
-      "pageNumber": 1,
-      "pageSize": 10,
-      "codeName": "Code de la propriété intellectuelle",
-      "sort": "TITLE_ASC"
-    }
-  }
-  
-numero = "2019-290"
-
-txt_complet_recup_identifiant = {
-    "recherche": {
-        "champs": [
-            {
-                "typeChamp": "NUM",
-                "criteres": [
-                    {
-                        "typeRecherche": "EXACTE",
-                        "valeur": f"{numero}",
-                        "operateur": "ET"
-                    }
-                ],
-                "operateur": "ET"
-            }
-        ],
-       "filtres": [
-            {
-                "facette": "DATE_VERSION",
-                "singleDate": 1561132975000
-            },
-            {
-                "facette": "TEXT_LEGAL_STATUS",
-                "valeur": "VIGUEUR"
-            }
-        ],
-        "pageNumber": 1,
-        "pageSize": 10,
-        "operateur": "ET",
-    "sort": "PERTINENCE",
-        "typePagination": "DEFAUT"
-    },
-   "fond": "LODA_ETAT"
-}
-
-code_complet = {
-    "recherche": {
-        "champs": [
-            {
-                "typeChamp": "ALL",
-                "criteres": [
-                    {
-                        "typeRecherche": "EXACTE",
-                        "valeur": "Fouille",
-                        "operateur": "ET"
-                    }
-                ],
-                "operateur": "ET"
-            }
-        ],
-       "filtres": [
-	       
-           
-            {
-                "facette": "TEXT_LEGAL_STATUS",
-                "valeur": "VIGUEUR"
-            }
-        ],
-        "pageNumber": 1,
-        "pageSize": 10,
-        "operateur": "ET",
-    "sort": "PERTINENCE",
-        "typePagination": "DEFAUT"
-    },
-   "fond": "LODA_ETAT"
-}
-
-table_matiere = {
-    
-  "nature": "CODE",
-  "textId": "LEGITEXT000006071366",
-  "date": "2021-04-15"
-}
-
-recup_loi_identifiant =  {
-  "date": 1561132975000,
-  "textId": "LEGITEXT000038359719"
-  }
-
-list_dossiers_legis = {
-  "legislatureId": 15,
-  "type": "LOI_PUBLIEE"
-}
-
-list_code = {
-  "states": [
-    "VIGUEUR",
-    "ABROGE",
-    "VIGUEUR_DIFF"
-  ],
-  "pageNumber": 1,
-  "pageSize": 10,
-  "codeName": "Code civil",
-  "sort": "TITLE_ASC"
-}
-
-consult_code = {
-  "textId": "LEGITEXT000006075116",
-  "searchedString": "constitution 1958",
-  "date": "2021-04-15",
-  "sctCid": "LEGISCTA000006112861"
-}
