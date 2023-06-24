ccda5a23147220451406cdb18cd8a5f9f6c3b936
rdassignies <93215594+rdassignies@users.noreply.github.com>
2023-06-24 18:48:33 +0200

    commit version 0.0.0.1

--- pylegifrance/api.py
+++ pylegifrance/api.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 18 09:13:33 2023
 
-@author: raphael
-"""
 
 import requests 
 import time
 import json
 from uuid import uuid4
-import inspect
-from inspect import Parameter
+from inspect import Signature, Parameter
 
 from requests_oauthlib import OAuth2Session
 
-_API_HOST = 'https://api.aife.economie.gouv.fr/dila/legifrance-beta/lf-engine-app'
+
+_API_HOST = ''
 _TOKEN_URL = 'https://oauth.piste.gouv.fr/api/oauth/token'
+
+#TODO : ajouterload env pour les var d'environnements
 
 """
 load_dotenv()
@@ -32,95 +30,94 @@ class LegiHandler:
         self.client_secret = "2bd78d98-d7fa-4835-9f61-fad6a7c41cd5" #mettre en env 0bc3dd0c-3c1c-431d-aab0-0ef2b0a9fb2c
         self.token_url = 'https://oauth.piste.gouv.fr/api/oauth/token'
         self.api_url = 'https://api.piste.gouv.fr/dila/legifrance/lf-engine-app/'
-        self.docs = [] #stock les dictionnaires de metadonnées
         self.time_token = time.time()
-        self._get_access_token()
+        self._get_access()
         
-        self.post_headers = {
-            'Authorization': f'Bearer {self.token}',
-            'accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
+    
+    def _get_access(self, attempts=3, delay=5):
         
-        from models import routes
-        self.routes = routes
-        
-        for route in routes.keys():
-            method_name = route.replace('/', '_').strip('_')  # Convertir la route en nom de méthode
-            method = self._create_method(route)
-            setattr(self, method_name, method)
-        
-        #with open(config_file, 'r') as f:
-        #    self.config = json.load(f)
-        
-    def _get_access_token(self):
         data = {
             'grant_type': 'client_credentials',
             'client_id': self.client_id,
             'client_secret': self.client_secret,
             'scope': 'openid'
         }
-        response = requests.post(self.token_url, data=data)
-        if 200 <= response.status_code < 300:
-                self.token = response.json().get('access_token')
-        else : 
-            raise Exception(f"Erreur lors de l'obtention du token erreur : {response.status_code}")
         
-    
-    
+        for i in range(attempts):
+            response = requests.post(self.token_url, data=data)
+            if 200 <= response.status_code < 300:
+                token = response.json().get('access_token')
+                self.token = token
+                self.expires_in = response.json().get('expires_in')
+                self.client = OAuth2Session(self.client_id, token=token)
+            else:
+                if i < attempts - 1:  # Si ce n'est pas la dernière tentative
+                    time.sleep(delay)  # Attendre avant la prochaine tentative
+                else:
+                    raise Exception(f"Erreur lors de l'obtention du token après {attempts} tentatives. Dernière erreur : {response.status_code} - {response.text}")
+        
     def _update_client(self) : 
+        
         elapsed_time = time.time() - self.time_token
-        if elapsed_time >= 3600 : 
+        print("elapsed time : ", elapsed_time)
+        if elapsed_time >= self.expires_in : 
+            print("renouvellement du token")
             self.time_token = time.time()
-            self.token = self._get_access_token()
+            self._get_access()
             
-            
-    def _create_method(self, route):
-        # Créer une nouvelle méthode qui appelle l'API avec les données fournies
-        def method(**data):
-            # Utiliser les valeurs par défaut de la route si une clé n'est pas présente dans les données
-            default_data = {key: data.get(key, value) for key, value in self.routes[route].items()}
-            # Si data est vide, utiliser default_data
-            if not data:
-                data = default_data
-            return self.call_api(route, data)
-        
-        # Créer une nouvelle signature pour la méthode avec les paramètres de la route
-        params = [Parameter(key, Parameter.KEYWORD_ONLY, default=value) for key, value in self.routes[route].items()]
-        method.__signature__ = inspect.Signature(params)
-
-        # Ajouter une docstring à la méthode
-        method.__doc__ = f"Appelle l'API avec la route '{route}' et les données suivantes : {self.routes[route]}"
-
-        return method
-
-
-    def _validate_data(self, route, data):
-        # Vérifiez que toutes les clés nécessaires sont présentes
-        for key in self.routes[route].keys():
-            if key not in data:
-                raise ValueError(f"La clé '{key}' est manquante dans" 
-                                 "les données fournies pour la route {route}")
-            if not data[key]:
-                raise ValueError(f"La clé '{key}' n'a pas de valeur associée"
-                                 "dans les données fournies pour la route {route}")
-   
         
     
-    def call_api(self, route, data):
-       
-        self._validate_data(route, data)
-        response = requests.post(f"{self.api_url}{route}",headers=self.post_headers, json=data)
+    def call_api(self, route=None, data=None): #TODO ajouter le type pour data
+            self._update_client()
+            headers = {
+                'Authorization': f'Bearer {self.token}',
+                'accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+            for d, i in data.items() : 
+                print('route', route, d, i, type(data))
+            
+            response = requests.post(f"{self.api_url}{route}",headers=headers, json=data)
+            
+            if response.status_code >= 400 and response.status_code < 500:
+                raise Exception(f"Erreur client {response.status_code} - {response.text} lors de l'appel à l'API :")
+      
+            return response
         
-        if response.status_code >= 400 and response.status_code < 500:
-            raise Exception(f"Erreur client  {response.status_code} lors de l'appel à l'API :")
-  
-        return response
     
+    class Method:
+        def __init__(self, func, parameters,route):
+            self._func = func
+            self.__signature__ = Signature(parameters)
+            self.route = route
 
+        def __call__(self, *args, **kwargs):
+            bound_values = self.__signature__.bind_partial(*args, **kwargs)
+            bound_values.apply_defaults()  # Apply default values for missing arguments
+            arguments = bound_values.arguments
+            #route = arguments.pop('route', self.route)  # extract 'route' from 'arguments'
+            
+            return self._func(route=self.route, data=arguments)
+    
+    def _add_endpoints(self):
+        
+        from models import endpoints
+
+        for endpoint, model in endpoints.items():
+            parameters = [
+                Parameter(name, Parameter.KEYWORD_ONLY, default=field.default)
+                for name, field in model.__fields__.items()
+            ]
+            #parameters.append(Parameter('route', Parameter.KEYWORD_ONLY, default=endpoint))
+
+            func_name = endpoint.replace('/', '_')
+            method = self.Method(self.call_api, parameters, endpoint)
+            setattr(self, func_name, method)
+    
+    
     def ping(self, route:str = "list/ping") : 
         pass
-
+    
     def get(self, route):
         headers = {
             'Authorization': f'Bearer {self.token}'
--- pylegifrance/models.py
+++ pylegifrance/models.py
@@ -1,46 +1,196 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr  2 12:14:21 2023
+from typing import Optional, List, Dict
+import re
+from enum import Enum
+from datetime import datetime
 
-@author: raphael
-"""
- 
-routes = {
-      "/consult/sameNumArticle": {
-        "textCid": "",
-        "articleCid": "",
-        "date": "",
-        "articleNum": ""
-      },
-      "/consult/legiPart": {
-        "searchedString": "",
-        "date": "",
-        "textId": ""
-      }, 
-      "/consult/juri": 
-      {
-       "textId": "JURITEXT000037999394",
-       "searchedString": "constitution 1958"
-       }, 
-      "/consult/jorf" : 
-      {
-       "textCid": "JORFTEXT000033736934",
-       "searchedString": "constitution 1958"
-       },
-      "/list/dossiersLegislatifs" : 
-         {
-             "legislatureId": 15,
-             "type": "LOI_PUBLIEE"
-         },
-    "/suggest" : 
-        {
-            "supplies": [
-                "JORF",
-                "JURI"
-        ],
-        "searchText": "mariage"
-        }
-        
-    }
+
+from pydantic import BaseModel, Field, validator
+from inspect import Signature, Parameter
+
+class JorfModel(BaseModel):
+    textCid:str = "JORFTEXT000033736934"
+    searchedString: Optional[str] = None
+
+    class Config:
+        route = "consult/jorf"
+
+
+class LegiPart(BaseModel):
+    date: str = "2021-04-15"
+    textId: str = "LEGITEXT000006075116"
+    searchedString: Optional[str] = None
+
+    class Config:
+        route = "consult/legiPart"
+
+
+
+
+class Fonds(str, Enum):
+    JORF = "JORF"
+    CNIL = "CNIL"
+    CETAT = "CETAT"
+    JURI = "JURI"
+    JUFI = "JUFI"
+    CONSTIT = "CONSTIT"
+    KALI = "KALI"
+    CODE_DATE = "CODE_DATE"
+    CODE_ETAT = "CODE_ETAT"
+    LODA_DATE = "LODA_DATE"
+    LODA_ETAT = "LODA_ETAT"
+    ALL = "ALL"
+    CIRC = "CIRC"
+    ACCO = "ACCO"
+
+class RechercheFonds(BaseModel):
+    fonds: Fonds
+    
+class Operateur(Enum):
+    """
+    Opérateur entre les champs de recherche
+
+    """
+    ET = "ET"
+    OU = "OU"
+
+class TypeRecherche(Enum):
+    EXACTE = "EXACTE"
+    UN_DES_MOTS = "UN_DES_MOTS"
+    TOUS_LES_MOTS_DANS_UN_CHAMP = "TOUS_LES_MOTS_DANS_UN_CHAMP"
+    AUCUN_DES_MOTS = "AUCUN_DES_MOTS"
+    AUCUNE_CORRESPONDANCE_A_CETTE_EXPRESSION = "AUCUNE_CORRESPONDANCE_A_CETTE_EXPRESSION"
+
+class TypeChamp(Enum):
+    ALL = "ALL"
+    TITLE = "TITLE"
+    TABLE = "TABLE"
+    NOR = "NOR"
+    NUM = "NUM"
+    ADVANCED_TEXTE_ID = "ADVANCED_TEXTE_ID"
+    NUM_DELIB = "NUM_DELIB"
+    NUM_DEC = "NUM_DEC"
+    NUM_ARTICLE = "NUM_ARTICLE"
+    ARTICLE = "ARTICLE"
+    MINISTERE = "MINISTERE"
+    VISA = "VISA"
+    NOTICE = "NOTICE"
+    VISA_NOTICE = "VISA_NOTICE"
+    TRAVAUX_PREP = "TRAVAUX_PREP"
+    SIGNATURE = "SIGNATURE"
+    NOTA = "NOTA"
+    NUM_AFFAIRE = "NUM_AFFAIRE"
+    ABSTRATS = "ABSTRATS"
+    RESUMES = "RESUMES"
+    TEXTE = "TEXTE"
+    ECLI = "ECLI"
+    NUM_LOI_DEF = "NUM_LOI_DEF"
+    TYPE_DECISION = "TYPE_DECISION"
+    NUMERO_INTERNE = "NUMERO_INTERNE"
+    REF_PUBLI = "REF_PUBLI"
+    RESUME_CIRC = "RESUME_CIRC"
+    TEXTE_REF = "TEXTE_REF"
+    TITRE_LOI_DEF = "TITRE_LOI_DEF"
+    RAISON_SOCIALE = "RAISON_SOCIALE"
+    MOTS_CLES = "MOTS_CLES"
+    IDCC = "IDCC"
+
+class CritereDTO(BaseModel):
+    proximite: Optional[int]
+    valeur: str
+    criteres: Optional[List["CritereDTO"]] = [] #Sous-critère/Sous-groupe de critères
+    typeRecherche: TypeRecherche
+    operateur: Optional[Operateur]
+
+class ChampDTO(BaseModel):
+    """ Objet décrivant une recherche dans un champ spécifique
+    
+    """
+    criteres: List[CritereDTO]
+    typeChamp: TypeChamp
+    operateur: Optional[Operateur] = None
+
+
+CritereDTO.update_forward_refs()
+
+
+class TypePagination(Enum):
+    DEFAUT = "DEFAUT"
+    ARTICLE = "ARTICLE"
+
+class DatePeriod(BaseModel):
+    end: Optional[str] = None
+    start: Optional[str] = None
+      
+    @validator('end', 'start')
+    def check_date_format(cls, v):
+        if v is not None:
+            if not re.match(r'\d{4}-\d{2}-\d{2}', v):
+                raise ValueError('singleDate must be in YYYY-MM-DD format')
+        return v
+    
+
+class FiltreDTO(BaseModel):
+    """
+    Objet décrivant un filtre de recherche
+
+
+    """
+    valeurs: Optional[List[str]] = None
+    multiValeurs: Optional[Dict[str, List[str]]] = None #Map des sous-valeur d'une valeur de filtre dans le cas d'un filtre par option texte. La clé doit être la valeur correspondante au parent dans la liste 'valeurs'
+    dates: Optional[DatePeriod] = None
+    facette: str
+    singleDate: Optional[str] = None
+    
+    @validator('singleDate')
+    def check_date_format(cls, v):
+        if v is not None:
+            if not re.match(r'\d{4}-\d{2}-\d{2}', v):
+                raise ValueError('singleDate must be in YYYY-MM-DD format')
+        return v
+
+class RechercheSpecifiqueDTO(BaseModel):
+    """ Objet permettant de créer une recherche
+    Args:
+        secondSort (Optional[str], default=None): Tri des éléments trouvés. Les tris possibles dépendent du fonds recherché.
+        champs (List[ChampDTO]): Liste des champs à rechercher.
+        filtres (Optional[List[FiltreDTO]], default=None): Liste des filtres à appliquer à la recherche.
+        fromAdvancedRecherche (Optional[bool], default=None): Indique si la recherche provient d'une recherche avancée.
+        typePagination (TypePagination): Type de pagination à utiliser pour la recherche.
+        pageNumber (int): Numéro de la page à récupérer.
+        pageSize (int): Nombre d'éléments par page.
+        sort (str): Champ sur lequel trier les résultats de la recherche.
+
+    """
+    secondSort:Optional[str] = Field(None, description="Tri des éléments trouvés (Les tris possibles dépendent du fonds recherché)")
+    champs: List[ChampDTO]
+    filtres: Optional[List[FiltreDTO]] = None
+    fromAdvancedRecherche: Optional[bool] = None
+    typePagination: TypePagination
+    pageNumber: int
+    pageSize: int
+    sort: str
+
+class SearchRequest(BaseModel):
+    """Requête paginée de recherche """
+    fond: Fonds
+    recherche: RechercheSpecifiqueDTO
+    
+class Recherche(BaseModel) : 
+    """Requête de recherche porteur du/des fonds ciblés, des filtres et des informations de pagination
+    """
+
+    recherche: SearchRequest
+    
+    
+endpoints = {
+    "consult/jorf": JorfModel,
+    "consult/legiPart": LegiPart,
+    "search" : Recherche, 
+}
+
+
+
+
+
+
 
